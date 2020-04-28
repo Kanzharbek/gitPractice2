8487d2823b0106d5845a93f0cf1284b0124906a8
Kanjar <kanjaranarbaev28@gmail.com>
2020-04-28 12:41:07 -0700

    add HappyBirthday.java

--- src/day1/AllGood.java
+++ src/day1/AllGood.java
@@ -3,5 +3,6 @@ package day1;
 public class AllGood {
     public static void main(String[] args) {
         System.out.println("All good");
+        System.out.println("New updates");
     }
 }
---
+++ src/day1/HappyBirthday.java
@@ -0,0 +1,7 @@
+package day1;
+
+public class HappyBirthday {
+    public static void main(String[] args) {
+        System.out.println("Happy Birthday!!!");
+    }
+}
