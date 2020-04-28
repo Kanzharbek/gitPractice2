0877b130c0e53dc900329066b707135ded7dad7b
Kanjar <kanjaranarbaev28@gmail.com>
2020-04-28 12:52:47 -0700

    add HappyBirthday.java

--- src/day1/HappyBirthday.java
+++ src/day1/HappyBirthday.java
@@ -3,5 +3,7 @@ package day1;
 public class HappyBirthday {
     public static void main(String[] args) {
         System.out.println("Happy Birthday!!!");
+        System.out.println("Happy Birthday Batch18");
+        // Happy Birthday
     }
 }
